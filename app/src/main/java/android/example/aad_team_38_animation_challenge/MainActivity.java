2839dc7382d61976e2bd6c057cf455c1b3f001e4
Efana Usua <usuaefana@gmail.com>
2019-10-15 09:30:07 +0100

    project setup

---
+++ app/src/main/java/android/example/aad_team_38_animation_challenge/MainActivity.java
@@ -0,0 +1,14 @@
+package android.example.aad_team_38_animation_challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
